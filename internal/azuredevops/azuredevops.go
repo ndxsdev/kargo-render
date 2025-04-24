206b41868bcd78ab3bf6043d3d73f5f9d05b34b6
Nathan Santos <nathan.santos@ab-inbev.com>
2025-04-24 16:15:57 -0300

    fix: azdevops

--- internal/azuredevops/azuredevops.go
+++ internal/azuredevops/azuredevops.go
@@ -5,10 +5,55 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/google/uuid"
 	"github.com/microsoft/azure-devops-go-api/azuredevops"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/git"
 	gitutil "github.com/akuity/kargo-render/pkg/git"
 )
+
+// parseAzureDevOpsURL parses an Azure DevOps repository URL and returns organization, project, and repository names
+func parseAzureDevOpsURL(repoURL string) (org, proj, repo string, err error) {
+	if strings.Contains(repoURL, "dev.azure.com") {
+		urlParts := strings.Split(strings.TrimPrefix(repoURL, "https://dev.azure.com/"), "/")
+		if len(urlParts) < 4 {
+			return "", "", "", fmt.Errorf("invalid Azure DevOps repository URL format")
+		}
+		org = urlParts[0]
+		proj = urlParts[1]
+		repo = strings.TrimSuffix(urlParts[3], ".git")
+	} else if strings.Contains(repoURL, ".visualstudio.com") {
+		urlParts := strings.Split(repoURL, "/")
+		if len(urlParts) < 5 {
+			return "", "", "", fmt.Errorf("invalid Azure DevOps repository URL format")
+		}
+		org = strings.Split(urlParts[2], ".")[0]
+		proj = urlParts[3]
+		repo = strings.TrimSuffix(urlParts[5], ".git")
+	} else {
+		return "", "", "", fmt.Errorf("unsupported Azure DevOps repository URL format")
+	}
+	return org, proj, repo, nil
+}
+
+// getRepositoryID gets the repository ID from Azure DevOps
+func getRepositoryID(ctx context.Context, client git.Client, project, repository string) (*uuid.UUID, error) {
+	repos, err := client.GetRepositories(ctx, git.GetRepositoriesArgs{
+		Project: &project,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error listing repositories: %w", err)
+	}
+
+	if repos != nil {
+		for _, repo := range *repos {
+			if *repo.Name == repository {
+				return repo.Id, nil
+			}
+		}
+	}
+
+	return nil, fmt.Errorf("repository '%s' not found in project '%s'", repository, project)
+}
 
 // OpenPR creates a pull request in Azure DevOps
 func OpenPR(
@@ -20,16 +65,16 @@ func OpenPR(
 	sourceBranch string,
 	creds gitutil.RepoCredentials,
 ) (string, error) {
-	// Parse Azure DevOps URL to extract organization and project
-	// Expected format: https://dev.azure.com/{organization}/{project}/_git/{repository}
-	urlParts := strings.Split(strings.TrimPrefix(repoURL, "https://dev.azure.com/"), "/")
-	if len(urlParts) < 4 {
-		return "", fmt.Errorf("invalid Azure DevOps repository URL format")
+	// Ensure we have a PAT token as password
+	if creds.Password == "" {
+		return "", fmt.Errorf("Azure DevOps requires a Personal Access Token (PAT) as password")
 	}
 
-	organization := urlParts[0]
-	project := urlParts[1]
-	repository := strings.TrimSuffix(urlParts[3], ".git")
+	// Parse Azure DevOps URL
+	organization, project, repository, err := parseAzureDevOpsURL(repoURL)
+	if err != nil {
+		return "", err
+	}
 
 	// Create a connection to Azure DevOps
 	connection := azuredevops.NewPatConnection(
@@ -37,10 +82,16 @@ func OpenPR(
 		creds.Password,
 	)
 
-	// Get Git client
+	// Create Git client
 	gitClient, err := git.NewClient(ctx, connection)
 	if err != nil {
 		return "", fmt.Errorf("error creating Azure DevOps Git client: %w", err)
+	}
+
+	// Get repository ID
+	repoUUID, err := getRepositoryID(ctx, gitClient, project, repository)
+	if err != nil {
+		return "", err
 	}
 
 	// Ensure branch names are in the correct format
@@ -50,14 +101,12 @@ func OpenPR(
 	// Create pull request
 	createPRArgs := git.CreatePullRequestArgs{
 		Project: &project,
+		RepositoryId: repoUUID,
 		GitPullRequestToCreate: &git.GitPullRequest{
 			Title:         &title,
 			Description:   &description,
 			SourceRefName: &sourceBranch,
 			TargetRefName: &targetBranch,
-			Repository: &git.GitRepository{
-				Name: &repository,
-			},
 		},
 	}
 
